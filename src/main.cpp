590664ee9ab6677ae349643719ee6c9bbe1649cc
Dan Lawrence <12dl38@queensu.ca>
2015-10-20 14:43:28 -0700

    add sfml to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,16 @@
-//testing one two three four
-//testing
+#include <SFML/Audio.hpp>
+#include <iostream>
+
+int main() {
+	sf::Music music;
+
+	if (!music.openFromFile("data/sounds/music.wav")) {
+		std::cout << "Error...";
+	}
+	else {
+		music.setLoop(true);
+		music.setVolume(50);
+		music.play();
+	}
+
+}
