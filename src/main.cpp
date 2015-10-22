255cd732e97062b19ab19691fb768beecffba0dd
Jonathan Turcotte <jonathanturcotte@outlook.com>
2015-10-22 19:51:32 -0400

    attempted to add taglib, not working

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 #include <SFML/Audio.hpp>
+#include <
 #include <iostream>
+
 int main() {
 	sf::Music music;
 
-	if (!music.openFromFile("../test/scholarships.flac")) {
+	if (!music.openFromFile("C:\\Users\\jonat\\University\\3rd_Year\\320\\Project\\walnut\\test\\scholarships.flac")) {
 		std::cout << "Unable to open music file";
 	}
 	else {
+		std::cout << "Playing!" << std::endl;
 		music.setLoop(true);
 		music.setVolume(50);
 		music.play();
+		getchar();
 	}
 }
