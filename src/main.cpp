53c30b21aa084ecdc9a2a1ab5e7e0e6a98e0c608
Jonathan Turcotte <jonathanturcotte@outlook.com>
2015-10-27 17:03:43 -0400

    commented sfml and taglib stuff to try and isolate source of build problems.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,31 @@
-#include <SFML/Audio.hpp>
+//#include <SFML/Audio.hpp> // causing me 390 external linker errors
+//#include <taglib/tag.h> not working yet
 #include <iostream>
+#include <string>
 
 int main() {
-	sf::Music music;
+	std::string filepath = "C:\\Users\\jonat\\University\\3rd_Year\\320\\Project\\walnut\\test\\scholarships.flac";
+	std::string input;
+	std::cout << "1. Test music playback" << std::endl << "2. Test tag reading" << std::endl << "Enter a number: ";
+	std::cin >> input;
+	std::cout << std::endl;
 
-	if (!music.openFromFile("C:\\Users\\jonat\\University\\3rd_Year\\320\\Project\\walnut\\test\\scholarships.flac")) {
-		std::cout << "Unable to open music file";
+	if (input == "1") {
+		/*sf::Music music;
+
+		if (!music.openFromFile(filepath)) {
+			std::cout << "Unable to open music file";
+		}
+		else {
+			std::cout << "Playing!" << std::endl;
+			music.setLoop(true);
+			music.setVolume(50);
+			music.play();
+			getchar();
+		}*/
+		std::cout << "testing1" << std::endl;
 	}
-	else {
-		std::cout << "Playing!" << std::endl;
-		music.setLoop(true);
-		music.setVolume(50);
-		music.play();
-		getchar();
+	else if (input == "2") {
+		std::cout << "testing2" << std::endl;
 	}
 }
