d0c121a75a60beed34008ba64e2f0b4b98bb76de
Jonathan Turcotte <jonathanturcotte@outlook.com>
2015-10-27 17:20:38 -0400

    added system("pause") to prevent immediate close of cmdline window

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,29 @@
 int main() {
 	std::string filepath = "C:\\Users\\jonat\\University\\3rd_Year\\320\\Project\\walnut\\test\\scholarships.flac";
 	std::string input;
-	std::cout << "1. Test music playback" << std::endl << "2. Test tag reading" << std::endl << "Enter a number: ";
-	std::cin >> input;
+	std::cout << "1. Test music playback" << std::endl << "2. Test tag reading" << std::endl;
 	std::cout << std::endl;
-
-	if (input == "1") {
-		/*sf::Music music;
-
-		if (!music.openFromFile(filepath)) {
-		std::cout << "Unable to open music file";
+	while (true) {
+		std::cout << "Enter a number: ";
+		std::cin >> input;
+		if (input == "1") {
+			std::cout << "testing1" << std::endl;
+			sf::Music music;
+			if (!music.openFromFile(filepath)) {
+				std::cout << "Unable to open music file";
+			}
+			else {
+				std::cout << "Playing!" << std::endl;
+				music.setLoop(true);
+				music.setVolume(50);
+				music.play();
+			}
+			break;
 		}
-		else {
-		std::cout << "Playing!" << std::endl;
-		music.setLoop(true);
-		music.setVolume(50);
-		music.play();
-		getchar();
-		}*/
-		std::cout << "testing1" << std::endl;
+		else if (input == "2") {
+			std::cout << "testing2" << std::endl;
+			break;
+		}
 	}
-	else if (input == "2") {
-		std::cout << "testing2" << std::endl;
-	}
+	system("pause");
 }
