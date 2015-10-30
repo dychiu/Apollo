aca1f6f31048ef173ec0252882bc8d559ff917a7
Jonathan Turcotte <jonathanturcotte@outlook.com>
2015-10-29 20:42:54 -0400

    Created a bit of testing framework.

--- SFML_Test/src/main.cpp
+++ SFML_Test/src/main.cpp
@@ -3,17 +3,39 @@
 
 int main()
 {
+	//Music object needs to be created here or it
+	//will go out of scope and be destroyed
 	sf::Music test;
+	std::string filepath = "C:\\Users\\jonat\\University\\3rd_Year\\320\\Project\\mpg123_test\\SFML_Test\\test\\scholarships.flac";
+	std::string input;
+	bool validInput = false;
+	std::cout << "1. Test generic music playback" << std::endl << "2. Test mp3 playback" << std::endl << "3. Test tag reading" << std::endl;
 
-	//Need to figure out relative filepaths, as they currently don't work.
-	if (!test.openFromFile("C:\\Users\\jonat\\University\\3rd_Year\\320\\Project\\walnut\\SFML_Test\\test\\test_ogg.ogg")) {
-		std::cout << "Unable to open music file";
-	}
-	else {
-		std::cout << "Playing!" << std::endl;
-		test.setLoop(true);
-		test.setVolume(50);
-		test.play();
+	while (!validInput) {
+		std::cout << "Enter a number: ";
+		std::cin >> input;
+		std::cout << std::endl;
+
+		if (input == "1") {
+			if (!test.openFromFile(filepath)) {
+				std::cout << "Unable to open music file";
+			}
+			else {
+				std::cout << "Playing!" << std::endl;
+				test.setLoop(true);
+				test.setVolume(50);
+				test.play();
+			}
+			validInput = true;
+		}
+		else if (input == "2") {
+			std::cout << "Option 2" << std::endl;
+			validInput = true;
+		}
+		else if (input == "3") {
+			std::cout << "Option 3" << std::endl;
+			validInput = true;
+		}
 	}
 	system("pause");
 	return 0;
