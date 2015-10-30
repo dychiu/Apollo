f68e2a7f5fdb53e2c0b634442e854b5f7c3dc427
Dan Lawrence <12dl38@queensu.ca>
2015-10-29 18:44:05 -0700

    Add taglib to project and remove debug execuatables from project. Dlls must be copied over from SFML_Test/dll

--- SFML_Test/src/main.cpp
+++ SFML_Test/src/main.cpp
@@ -1,12 +1,14 @@
 #include <SFML/Audio.hpp>
 #include <iostream>
+#include <taglib/tag.h>
+#include <taglib/fileref.h>
 
 int main()
 {
 	//Music object needs to be created here or it
 	//will go out of scope and be destroyed
 	sf::Music test;
-	std::string filepath = "C:\\Users\\jonat\\University\\3rd_Year\\320\\Project\\mpg123_test\\SFML_Test\\test\\scholarships.flac";
+	std::string filepath = "../SFML_Test/test/scholarships.flac";
 	std::string input;
 	bool validInput = false;
 	std::cout << "1. Test generic music playback" << std::endl << "2. Test mp3 playback" << std::endl << "3. Test tag reading" << std::endl;
@@ -34,7 +36,12 @@ int main()
 		}
 		else if (input == "3") {
 			std::cout << "Option 3" << std::endl;
+			TagLib::FileRef f(filepath.c_str());
+			TagLib::String artist = f.tag()->artist();
+			std::cout << "Artist is: " << artist << std::endl;
+			system("pause");
 			validInput = true;
+
 		}
 	}
 	system("pause");
