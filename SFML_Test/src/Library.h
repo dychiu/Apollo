e2e73d3b4dee643022ac8a27fc76ef1e9137258d
Jonathan Turcotte <jonathanturcotte@outlook.com>
2015-11-05 16:45:41 -0500

    defined MusicPlayer.h, Library.h

--- SFML_Test/src/Library.h
+++ SFML_Test/src/Library.h
@@ -1,1 +1,26 @@
 #pragma once
+
+#include "MusicPlayer.h"
+#include "artist.h"
+#include "album.h"
+#include "Song.h"
+#include "boost\filesystem.hpp"
+#include <vector>
+
+namespace fs = boost::filesystem;
+
+class Library {
+private:
+	std::vector<Artist> artistList;
+	std::vector<Song> songList;
+	std::string xmlFilepath;
+	std::string musicDirectory;
+
+	std::vector<Artist> importLibrary();			//Import from musicDirectory
+	std::vector<Artist> importXML();				//Import from xmlFilepath 
+public:
+	Library();	
+	void load();		//Checks xmlFilepath
+	void save();		//Saves to xmlFilepath
+	void updateLibrary(); //calls importLibrary()
+};
--- SFML_Test/src/MusicPlayer.h
+++ SFML_Test/src/MusicPlayer.h
@@ -1,1 +1,33 @@
 #pragma once
+
+#include "Library.h"
+#include "song.h"
+#include "album.h"
+#include "artist.h"
+#include "GUI.h"
+
+#include "SFML/Audio.hpp"
+
+
+
+class MediaPlayer {
+private:
+	Library library;
+	Song currentSong;
+	Album currentAlbum;
+	Artist currentArtist;
+
+public:
+	MediaPlayer(); //Creates GUI
+	~MediaPlayer();
+	void playSong(std::string filepath);
+	void pauseSong(sf::Music);
+	void closeSong(sf::Music);
+	void setVolume(sf::Music);
+	void seekSong(sf::Music);
+
+	//Accessors
+	Song getCurrentSong();
+	Album getCurrentAlbum();
+	Artist getCurrentArtist();
+};
