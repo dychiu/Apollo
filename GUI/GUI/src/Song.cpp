f62b81d5163d0a5e6099b2a208598075f260fdd0
Dylan Chiu <dylan.chiu@hotmail.com>
2015-11-19 19:22:08 -0500

    Completed Song.h and Song.cpp

--- GUI/GUI/src/Song.cpp
+++ GUI/GUI/src/Song.cpp
@@ -4,6 +4,48 @@ Song::Song() {
 
 }
 
-Song::Song(System::String^ n) {
-	name = n;
+Song::Song(TagLib::File^ tagFile, System::String^ path) {
+	filePath = path;
+	songName = tagFile->Tag->Title;
+	genre = tagFile->Tag->FirstGenre;
+	trackNumber = tagFile->Tag->Track;
+	bpm = tagFile->Tag->BeatsPerMinute;
+}
+
+void Song::setParentArtist(Artist^ artist) {
+	parentArtist = artist;
+	return;
+}
+
+void Song::setParentAlbum(Album^ album) {
+	parentAlbum = album;
+	return;
+}
+
+System::String^ Song::getSongName() {
+	return songName;
+}
+
+System::String^ Song::getGenre() {
+	return genre;
+}
+
+System::String^ Song::getFilePath() {
+	return filePath;
+}
+
+int Song::getTrackNumber() {
+	return trackNumber;
+}
+
+int Song::getBPM() {
+	return bpm;
+}
+
+Artist^ Song::getParentArtist() {
+	return parentArtist;
+}
+
+Album^ Song::getParentAlbum() {
+	return parentAlbum;
 }
--- GUI/GUI/src/Song.h
+++ GUI/GUI/src/Song.h
@@ -1,16 +1,29 @@
 #pragma once
 
+#include "Artist.h"
+#include "Album.h"
+
 public ref class Song {
 public:
 	Song();
-	Song(System::String^ n);
+	Song(TagLib::File^ tagFile, System::String^ path);
+	
+	void setParentArtist(Artist^ artist);
+	void setParentAlbum(Album^ album);
 
-	property System::String^ SongName {
-		System::String^ get() {
-			return name;
-		}
-	}
-
+	System::String^ getSongName();
+	System::String^ getGenre();
+	System::String^ getFilePath();
+	int getTrackNumber();
+	int getBPM();
+	Artist^ getParentArtist();
+	Album^ getParentAlbum();
 private:
-	System::String^ name;
+	System::String^ songName;
+	System::String^ genre;
+	System::String^ filePath;
+	int trackNumber;
+	int bpm;
+	Artist^ parentArtist;
+	Album^ parentAlbum;
 };
